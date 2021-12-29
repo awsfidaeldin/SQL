355993e826d7d8f26277886ef34963c78af05f48
Aws Yousif <aws.yousif@gmail.com>
2021-12-29 13:22:51 +0300

    Add files via upload

---
+++ CreateAccount.sql
@@ -0,0 +1,2 @@
+Create user AXXXXXXXXX identified by AXXXXXXXXX;
+grant create session, create table, create view, create sequence, create any index,CREATE PROCEDURE,CREATE TRIGGER, unlimited tablespace to AXXXXXXXXX;

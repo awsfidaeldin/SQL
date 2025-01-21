574e87e006c0ef6a37a5f0a248a084585d510e14
Aws Yousif <aws.yousif@gmail.com>
2025-01-21 14:44:48 +0300

    Update CreateAccount.sql

--- CreateAccount.sql
+++ CreateAccount.sql
@@ -1,2 +1,4 @@
 Create user AXXXXXXXXX identified by AXXXXXXXXX;
 grant create session, create table, create view, create sequence, create any index,CREATE PROCEDURE,CREATE TRIGGER, unlimited tablespace to AXXXXXXXXX;
+
+ALTER USER sa1703156 IDENTIFIED BY sa1703156 ACCOUNT UNLOCK;
